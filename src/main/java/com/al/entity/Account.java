f709af3846e9081439fe7e00c39ecbb8b7eff27e
45atharvlugade <lugadeatharv8@gmail.com>
2026-05-23 13:35:26 +0530

    Account entity

---
+++ src/main/java/com/al/entity/Account.java
@@ -0,0 +1,72 @@
+package com.al.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name="account_jpa")
+@Getter
+@Setter
+public class Account {
+
+	@Id
+	@SequenceGenerator(name = "accountSeq",sequenceName = "account_sequence",initialValue = 1000000000,allocationSize =3)
+	@GeneratedValue(generator = "accountSeq",strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	private AccountType type;
+	
+	private Double balance;
+	
+	@NonNull
+	@Column(nullable = false)
+	private AccountStatus status;
+	
+	// MetaData Columns
+	
+	@CreationTimestamp
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	@Column(insertable = false)
+	private LocalDateTime updateAt;
+	
+	@Version
+	private int updateCount;
+	
+	
+	// Entity Relations
+	
+	@OneToOne(orphanRemoval = true,cascade = CascadeType.ALL)
+	@JoinColumn(referencedColumnName = "userId",name = "user_id")
+	private User user;
+	
+	@OneToMany(mappedBy = "account")
+	private List<Transaction> transactions;
+	
+	@OneToMany(mappedBy = "account")
+	private List<AccountLedger> accountLedgers;
+}
